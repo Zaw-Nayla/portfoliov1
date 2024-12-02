331dc2b535f6a76538d7d4f5fc5928320cf2050f
Zaw Nay La <zawnayla.znl@gmail.com>
2024-12-02 10:55:27 +0630

    Finished initial plan-2

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ import Contact from "./Contact";
 import Experience from "./Experience";
 import Hero from "./Hero";
 import Milestones from "./Milestones";
-// import CanvasLoader from "./Loader";
 import Navbar from "./NavBar";
 import Position from "./Position";
 
